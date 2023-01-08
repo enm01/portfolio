e0a5fbf1103df7f1282373928f2031b3daedc527
imane <imane.ziani1@ump.ac.ma>
2023-01-08 20:15:06 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+    {
+      title: "Quiz Application",
+      subtitle: "using : ReactJs / Material UI / Trivia API ",
+      description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://github.com/enm01/",
+    },
+    {
+        title: "Blood collection process management application",
+        subtitle: "using : HTML / CSS / BootStrap / PHP ",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+        link: "https://github.com/enm01/",
+      },
+      {
+        title: "Blood collection process management application",
+        subtitle: "using : HTML / CSS / BootStrap / PHP ",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+        link: "https://github.com/enm01/",
+      },
+      {
+        title: "Quiz Application",
+        subtitle: "using : ReactJs / Material UI / Trivia API ",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://github.com/enm01/",
+      },
+];
+export const skills = ["HTML","CSS" ,"Bootstrap","Material UI","JavaScript"  ,"ReactJs" ,"PHP" ,"JAVA" , "JEE"];
+    
+        
+        
+    
+
+
